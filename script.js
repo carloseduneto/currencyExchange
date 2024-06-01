50c6883b3a1f039a2514ef68a995fa87a21ca3c5
Carlos Neto <112590775+carloseduneto@users.noreply.github.com>
2024-06-01 00:45:36 -0300

    feat: :art:

--- script.js
+++ script.js
@@ -1,6 +1,188 @@
 let apiVersion = "v1"
 // câmbio monetário
 // exchange currency
+
+let currencyExchangePTBR = {
+"AFN":"Afegane Afegão",
+"MGA":"Ariary",
+"THB":"Baht",
+"PAB":"Balboa",
+"ETB":"Birr etíope",
+"VES":"Bolívar Soberano Venezuelano",
+"BOB":"Boliviano",
+"BOV":"Boliviano Mvdol",
+"GHS":"Cedi",
+"CRC":"Colon da Costa Rica",
+"SVC":"Colon de El Salvador",
+"NIO":"Cordoba Oro",
+"CZK":"Coroa checa",
+"DKK":"Coroa dinamarquesa",
+"ISK":"Coroa islandesa",
+"NOK":"Coroa norueguesa",
+"SEK":"Coroa Sueca",
+"GMD":"Dalasi",
+"MKD":"Denar",
+"DZD":"Dinar argelino",
+"BHD":"Dinar do Bahrein",
+"KWD":"Dinar do Kuwait",
+"IQD":"Dinar iraquiano",
+"JOD":"Dinar jordano",
+"LYD":"Dinar líbio",
+"RSD":"Dinar Sérvio",
+"SDG":"Dinar sudanês",
+"TND":"Dinar tunisino",
+"AED":"Dirham dos Emirados",
+"MAD":"Dirham marroquino",
+"STN":"Dobra",
+"USD":"Dólar americano",
+"AUD":"Dólar australiano",
+"BMD":"Dólar bermudense",
+"CAD":"Dólar canadense",
+"GYD":"Dólar da Guiana",
+"NAD":"Dólar da Namíbia",
+"NZD":"Dólar da Nova Zelândia",
+"BSD":"Dólar das Bahamas",
+"XCD":"Dólar das Caraíbas Orientais",
+"FJD":"Dólar das Fiji",
+"KYD":"Dólar das Ilhas Cayman",
+"SBD":"Dólar das Ilhas Salomão",
+"BBD":"Dólar de Barbados",
+"HKD":"Dólar de Hong Kong",
+"SGD":"Dólar de Singapura",
+"TTD":"Dólar de Trindade e Tobago",
+"BZD":"Dólar do Belize",
+"BND":"Dólar do Brunei",
+"SRD":"Dólar do Suriname",
+"JMD":"Dólar jamaicano",
+"LRD":"Dólar liberiano",
+"ZWL":"Dólar zimbabuano",
+"VND":"Dong",
+"AMD":"Dram",
+"CVE":"Escudo cabo-verdiano",
+"EUR":"Euro",
+"XBA":"European Composite Unit (EURCO)",
+"XBB":"European Monetary Unit (E.M.U.-6)",
+"XBD":"European Unit of Account 17 (E.U.A.-17)",
+"XBC":"European Unit of Account 9 (E.U.A.-9)",
+"ANG":"Florim",
+"AWG":"Florim de Aruba",
+"HUF":"Forint",
+"XAF":"Franco CFA central",
+"XOF":"Franco CFA ocidental",
+"XPF":"Franco CFP",
+"KMF":"Franco comoriano",
+"CDF":"Franco congolês",
+"GNF":"Franco da Guiné",
+"BIF":"Franco do Burundi",
+"DJF":"Franco do Djibuti",
+"RWF":"Franco do Ruanda",
+"CHF":"Franco suíço",
+"HTG":"Gourde",
+"UAH":"Grívnia",
+"PYG":"Guarani",
+"JPY":"Iene",
+"PGK":"Kina",
+"LAK":"Kip",
+"MWK":"Kwacha",
+"ZMW":"Kwacha",
+"AOA":"Kwanza",
+"MMK":"Kyat",
+"GEL":"Lari",
+"LVL":"Lats",
+"ALL":"Lek",
+"HNL":"Lempira",
+"SLL":"Leone",
+"MDL":"Leu moldávio",
+"RON":"Leu romeno",
+"BGN":"Lev búlgaro",
+"SYP":"Libra da Síria",
+"FKP":"Libra das Malvinas",
+"GIP":"Libra de Gibraltar",
+"SHP":"Libra de Santa Helena",
+"EGP":"Libra egípcia",
+"GBP":"Libra Esterlina",
+"LBP":"Libra libanesa",
+"IMP":"Libra manesa",
+"SZL":"Lilangeni",
+"LTL":"Litas",
+"LSL":"Loti",
+"AZN":"Manat do Azerbaijão",
+"TMT":"Manat turcomano",
+"BAM":"Marco convertível",
+"MZN":"Metical",
+"NGN":"Naira",
+"ERN":"Nakfa",
+"BTN":"Ngultrum",
+"TRY":"Nova Lira turca",
+"TWD":"Novo dólar de Taiwan",
+"PEN":"Nuevo Sol",
+"MRO":"Ouguiya",
+"XAU":"Ouro",
+"TOP":"Pa'anga",
+"XPD":"Paládio",
+"MOP":"Pataca",
+"DOP":"Peso",
+"ARS":"Peso Argentino",
+"CLP":"Peso chileno",
+"COP":"Peso colombiano",
+"CUP":"Peso cubano",
+"PHP":"Peso filipino",
+"MXN":"Peso mexicano",
+"UYU":"Peso uruguaio",
+"UYI":"Peso uruguaio em Unidades Indexadas",
+"XPT":"Platina",
+"XAG":"Prata",
+"BWP":"Pula",
+"GTQ":"Quetzal guatemalteco",
+"ZAR":"Rand",
+"BRL":"Real Brasileiro",
+"CNY":"Renminbi",
+"QAR":"Rial do Qatar",
+"YER":"Rial iemenita",
+"IRR":"Rial iraniano",
+"OMR":"Rial Omani",
+"KHR":"Riel",
+"MYR":"Ringgit",
+"SAR":"Riyal",
+"RUB":"Rublo",
+"BYR":"Rublo bielorrusso",
+"MVR":"Rufiyaa",
+"SCR":"Rupia das Seychelles",
+"LKR":"Rupia do Sri Lanka",
+"INR":"Rupia indiana",
+"IDR":"Rupia indonésia",
+"MUR":"Rupia mauriciana",
+"NPR":"Rupia nepalesa",
+"PKR":"Rupia paquistanesa",
+"ILS":"Shekel",
+"KGS":"Som",
+"UZS":"Som uzbeque",
+"TJS":"Somoni",
+"XDR":"Special Drawing Rights",
+"ECS":"Sucre",
+"BDT":"Taka",
+"WST":"Tala",
+"KZT":"Tenge",
+"MNT":"Tugrik",
+"XFU":"UIC franc (moeda de liquidação especial)",
+"CLF":"Unidade de Fomento (código de fundos)",
+"COU":"Unidade de Valor Real",
+"MXV":"Unidade Mexicana de Investimento",
+"VUV":"Vatu",
+"CHE":"WIR euro (moeda complementar)",
+"CHW":"WIR franc (moeda complementar)",
+"KPW":"Won norte-coreano",
+"KRW":"Won sul-coreano",
+"TZS":"Xelim da Tanzânia",
+"KES":"Xelim queniano",
+"SOS":"Xelim somali",
+"UGX":"Xelim ugandês",
+"PLN":"Zloty"
+}
+
+
+
+
 async function currencyExchange(exchange, exchange2, date) {
     // let date = "latest"; //or YYYY-MM-DD
     let endpoint = "currencies/"+exchange
@@ -48,21 +230,67 @@ async function getFullCurrencies() {
 
     try {
         const valueFromAPI = await fetch(apiURL)
-        const dataJSON = await valueFromAPI.json()
-        let dataJSONKeys = Object.keys(dataJSON)
-        let dataJSONValues = Object.values(dataJSON)
-        let options = ""
-        //` <option value="" selected disabled hidden>Selecione uma moeda</option>`;
+        let dataJSON = await valueFromAPI.json()
 
+        let options = ""
+        let newData 
+        let newData2
+
+        let type = "common"
+
+        replaceJSONValues(dataJSON, currencyExchangePTBR)
+        newData = inverterChavesValores(dataJSON)
+        newData2 = ordenarJSONAlfabeticamente(newData)
+
+        let dataJSONKeys = Object.values(newData2)
+        let dataJSONValues = Object.keys(newData2)
+
+
+        console.log(currencyExchangePTBR)
+        console.log(dataJSON)
+        console.log(newData)
+
+
+
+
+        // ` <option value="" selected disabled hidden>Selecione uma moeda</option>`;
+
+        /*let dataConvertered = Object.entries(dataJSON)
+
+        dataConvertered.sort(([, valueA], [, valueB]) => {
+            return valueA.localeCompare(valueB);
+        });
+
+        dataJSON = Object.fromEntries(dataConvertered)*/
+
+        /*
+        
+        Object.entries(currencyExchangePTBR).forEach(([keyPTBR, valuePTBR]) => {
+            // console.log(`${keyPTBR}: ${valuePTBR}`);
+            if (keys == keyPTBR) {
+                // console.log(keys)
+                values = valuePTBR
+            }
+        });*/
         for (let i = 0; i < dataJSONKeys.length; i++) {
             const keys = dataJSONKeys[i].toUpperCase();
-            const values = dataJSONValues[i]
-            /*if(values != ""){
-                options += `<option value="${(keys)}">${(values)}</option>` 
+            let values = dataJSONValues[i]
+            
+            if (values != "") {
+                if (type == "common") {
+                    if (currencyExchangePTBR.hasOwnProperty(keys)) {
+                        options += `<input type="button" value="${(values)}" name="${(keys)}" class="button"> <br>`
+                    }
+                } else if (type == "crypto") {
+                    if (!currencyExchangePTBR.hasOwnProperty(keys)) {
+                        options += `<input type="button" value="${(values)}" name="${(keys)}" class="button"> <br>`
+                    }
+                }
+                // options += `<option value="${(keys)}">${(values)}</option>` 
                 // console.log(keys + " " + values)
+            }
 
-            }*/
-
+            /*
             if(values !=""){
                 options +=  `
                 <div >
@@ -70,15 +298,15 @@ async function getFullCurrencies() {
                 <label for="huey">${(values)}</label>
             </div>
                 `
-            }
+            }*/
         }
 
-        /*currency1.innerHTML = options
-        console.log(options)
+        // currency1.innerHTML = options
+        // console.log(options)
         currencies[0].innerHTML = options
-        currencies[1].innerHTML = options
-        currencies[2].innerHTML = options*/
-        currencies[3].innerHTML = options
+        // currencies[1].innerHTML = options
+        // currencies[2].innerHTML = options
+        // currencies[3].innerHTML = options
 
 
 
@@ -90,6 +318,46 @@ async function getFullCurrencies() {
 
 }
 
+function replaceJSONValues(json1, json2) {
+    const json2LowerCaseKeys = {}
+    for (let key in json2) {
+        json2LowerCaseKeys[key.toLowerCase()] = json2[key]
+    }
+
+    for (let key in json1) {
+        const keyLowerCase = key.toLowerCase()
+        if (json2LowerCaseKeys.hasOwnProperty(keyLowerCase)) {
+            json1[key] = json2LowerCaseKeys[key]
+        }
+    }
+}
+
+
+function inverterChavesValores(objeto) {
+    const novoObjeto = {};
+    for (const chave in objeto) {
+        if (objeto.hasOwnProperty(chave)) {
+            novoObjeto[objeto[chave]] = chave;
+        }
+    }
+    return novoObjeto;
+}
+
+
+
+function ordenarJSONAlfabeticamente(objeto) {
+    const chavesOrdenadas = Object.keys(objeto).sort();
+    const objetoOrdenado = {};
+    
+    chavesOrdenadas.forEach(chave => {
+        objetoOrdenado[chave] = objeto[chave];
+    });
+
+    return objetoOrdenado;
+}
+
+
+/*
 
 function changeCurrency1(){
 
@@ -107,9 +375,23 @@ $(document).ready(function() {
     });
 });
 
-
+*/
 document.querySelectorAll('input[name="drone"]').forEach((radio) => {
     radio.addEventListener('change', function() {
         document.getElementById('selectedValue').textContent = `Selected value: ${this.value}`;
     });
 });
+
+        // Seleciona o elemento dialog
+        const modal = document.querySelector('.money');
+
+        // Adiciona um event listener ao contêiner (modal)
+        modal.addEventListener('click', function(event) {
+            // Verifica se o elemento clicado é um botão de entrada
+            if (event.target.tagName === 'INPUT' && event.target.type === 'button') {
+                // Recupera o valor do botão clicado
+                const buttonValue = event.target.name;
+                // Exibe o valor no console
+                console.log('Botão clicado:', buttonValue);
+            }
+        });
